047229d83c5a415cc2d493a10a54e4a1b94809fd
hamzahabdo <alhamza1997@gmail.com>
2026-01-20 14:13:39 +0300

    Add WT Equipemtn table

---
+++ wt_operations/wt_operations/doctype/wt_equipment/wt_equipment.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, Takamol and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class WTEquipment(Document):
+	pass
